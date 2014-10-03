903ed6697e40ce6caa6c7e1b8e378b79c25c4af8
Tony Morris <tmorris@tmorris.net>
2014-10-03 15:24:33 +1000

    parser

--- src/Data/Geo/Coordinate/Parse.hs
+++ src/Data/Geo/Coordinate/Parse.hs
@@ -3,17 +3,11 @@ module Data.Geo.Coordinate.Parse where
 import Text.Parser.Char
 import Text.Parser.Combinators
 import Prelude hiding (elem)
-import Data.Functor
-import Control.Applicative(Alternative((<|>)), Applicative(..))
+import Control.Applicative(Alternative((<|>)), liftA3)
 import Data.Foldable(elem, asum)
 import Text.Parsec(parse)
 import Data.Char
 import Data.Bool
-
-data Sign =
-  Neg
-  | Pos
-  deriving (Eq, Show)
 
 skipChars ::
   CharParsing f =>
@@ -21,30 +15,32 @@ skipChars ::
 skipChars =
   asum (fmap char "?*")
    
-dig ::
+satisfyInteger ::
   (CharParsing p, Monad p) =>
   (Int -> Bool)
   -> p Int  
-dig p =
-  let digit' = fmap digitToInt digit
-      f <<*>> x = f bool return <*> dig' <*> p $ x
-      dig' n =
+satisfyInteger p =
+  let digit' =
+        fmap digitToInt digit
+      sat n =
         try (do c <- digit'
-                (\b -> pure . b . ($ n)) <<*>> (n * 10 + c)) <|> return n
+                bool3 (n * 10 + c)) <|> return n
+      bool3 =
+        liftA3 bool return sat p
   in do sn <- optional (char '-' <|> char '+') 
         c1 <- digit'
-        d <- (<$>) <<*>> c1
+        d  <- bool3 c1
         return (bool id negate (elem '-' sn) d)
 
 dg90 ::
  (CharParsing f, Monad f) =>
   f Int
 dg90 =
-  dig (< 90)
+  satisfyInteger (\n -> n <= 8)
 
 dg180 ::
  (CharParsing f, Monad f) =>
   f Int
 dg180 =
-  dig (< 180)
+  satisfyInteger (< 180)
  
