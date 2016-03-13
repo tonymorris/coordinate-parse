9c63ee96ae52c173b4c8ec0b07af4959eb1ce86f
Tony Morris <tmorris@tmorris.net>
2016-03-14 08:34:27 +1000

    exp

--- src/Data/Geo/Coordinate/Parse.hs
+++ src/Data/Geo/Coordinate/Parse.hs
@@ -3,7 +3,7 @@ module Data.Geo.Coordinate.Parse where
 import Text.Parser.Char
 import Text.Parser.Combinators
 import Prelude hiding (elem)
-import Control.Applicative(Alternative((<|>)), liftA3)
+import Control.Applicative(Alternative((<|>)), liftA3, (<$>))
 import Data.Foldable(elem, asum)
 import Text.Parsec(parse)
 import Data.Char
@@ -21,20 +21,29 @@ satisfyInteger ::
   (Int -> Bool)
   -> p Int  
 satisfyInteger p =
+  let digitP n g =
+        do c <- digit
+           liftA3 bool return (\n -> try (digitP n g) <|> return n) p (n * 10 + g (digitToInt c))
+  in bool id negate . elem '-' <$> optional (char '-' <|> char '+') >>=
+       digitP 0
+
+satisfyNatural ::
+  (CharParsing p, Monad p) =>
+  (Int -> Bool)
+  -> p Int  
+satisfyNatural p =
   let sat n =
         try (digitP n) <|> return n
       digitP n =
         do c <- digit
            liftA3 bool return sat p (n * 10 + digitToInt c)
-  in do sn <- optional (char '-' <|> char '+') 
-        d  <- digitP 0 
-        return (bool id negate (elem '-' sn) d)
-
+  in digitP 0 
+        
 parseDegreesLatitude ::
  (CharParsing f, Monad f) =>
   f DegreesLatitude
 parseDegreesLatitude =
-  fmap remDegreesLatitude (satisfyInteger (<= 8))
+  fmap remDegreesLatitude (satisfyInteger (\n -> all ($n) [(>= -8), (<= 8)]))
 
 parseDegreesLongitude ::
  (CharParsing f, Monad f) =>
